bf76d71313d3b75c47a01bc75728dff5ca16b842
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-06 18:13:17 -0300

    chore: initial commit

---
+++ src/main/java/com/alan/api_autores_obras/ApiAutoresObrasApplication.java
@@ -0,0 +1,13 @@
+package com.alan.api_autores_obras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAutoresObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAutoresObrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alan/api_autores_obras/ApiAutoresObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.api_autores_obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutoresObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
