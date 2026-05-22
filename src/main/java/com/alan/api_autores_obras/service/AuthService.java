8f28146d812e76eee6e699f8f9f54b56b914aaa1
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-22 19:53:31 -0300

    feat: add ConflictException

---
+++ src/main/java/com/alan/api_autores_obras/service/AuthService.java
@@ -0,0 +1,26 @@
+package com.alan.api_autores_obras.service;
+
+import com.alan.api_autores_obras.dto.auth.CadastroRequest;
+import com.alan.api_autores_obras.entity.Usuario;
+import com.alan.api_autores_obras.repository.UsuarioRepository;
+import com.alan.api_autores_obras.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+
+    public Usuario cadastrarUsuario(CadastroRequest request){
+        Usuario
+    }
+
+
+}
