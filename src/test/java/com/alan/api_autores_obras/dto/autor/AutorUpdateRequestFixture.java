70655c8695cbbc1fc94ccd4246a1b698c3c61bfc
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-24 19:05:24 -0300

    feat: add AutorUpdateRequestFixture

--- src/test/java/com/alan/api_autores_obras/dto/autor/AutorUpdateRequestFixture.java
+++ src/test/java/com/alan/api_autores_obras/dto/autor/AutorUpdateRequestFixture.java
@@ -1,4 +1,20 @@
 package com.alan.api_autores_obras.dto.autor;
 
+import com.alan.api_autores_obras.enums.Sexo;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class AutorUpdateRequestFixture {
+    public static AutorUpdateRequest build(){
+        return AutorUpdateRequest.builder()
+                .nome("Machado de Assis")
+                .sexo(Sexo.MASCULINO)
+                .email("machado@assis.com")
+                .dataNascimento(LocalDate.of(1839, 6, 21))
+                .paisDeOrigem("Brasil")
+                .cpf("123.456.789-00")
+                .obrasId(List.of())
+                .build();
+    }
 }
