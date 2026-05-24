2f1a78918e5abcd9a9ef13c507097aaba9bbff2e
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-24 19:02:15 -0300

    feat: add AutorResumeResponseFixture

--- src/test/java/com/alan/api_autores_obras/dto/autor/AutorResumeResponseFixture.java
+++ src/test/java/com/alan/api_autores_obras/dto/autor/AutorResumeResponseFixture.java
@@ -1,4 +1,14 @@
 package com.alan.api_autores_obras.dto.autor;
 
+import java.time.LocalDate;
+
 public class AutorResumeResponseFixture {
+    public static AutorResumeResponse build(){
+        return AutorResumeResponse.builder()
+                .id(1L)
+                .nome("Machado de Assis")
+                .dataNascimento(LocalDate.of(1839, 6, 21))
+                .paisDeOrigem("Brasil")
+                .build();
+    }
 }
