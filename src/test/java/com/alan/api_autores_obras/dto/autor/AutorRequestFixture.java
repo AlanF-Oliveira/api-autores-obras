71c22539819fe8b45ab849d6553b4f7cefe337e8
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-05-24 18:59:02 -0300

    feat: add AutorRequestFixture and AutorResponseFixture

--- src/test/java/com/alan/api_autores_obras/dto/autor/AutorRequestFixture.java
+++ src/test/java/com/alan/api_autores_obras/dto/autor/AutorRequestFixture.java
@@ -1,4 +1,23 @@
 package com.alan.api_autores_obras.dto.autor;
 
+import com.alan.api_autores_obras.enums.Sexo;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class AutorRequestFixture {
+    public static AutorRequest build() {
+        return AutorRequest.builder()
+                .nome("Machado de Assis")
+                .sexo(Sexo.MASCULINO)
+                .email("machado@assis.com")
+                .dataNascimento(LocalDate.of(1839, 6, 21))
+                .paisDeOrigem("Brasil")
+                .cpf("123.456.789-00")
+                .obrasId(List.of())
+                .build();
+    }
+
+
 }
+
--- src/test/java/com/alan/api_autores_obras/dto/autor/AutorResponseFixture.java
+++ src/test/java/com/alan/api_autores_obras/dto/autor/AutorResponseFixture.java
@@ -1,4 +1,21 @@
 package com.alan.api_autores_obras.dto.autor;
 
+import com.alan.api_autores_obras.enums.Sexo;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class AutorResponseFixture {
+    public static AutorResponse build(){
+        return AutorResponse.builder()
+                .nome("Machado de Assis")
+                .sexo(Sexo.MASCULINO)
+                .email("machado@assis.com")
+                .dataNascimento(LocalDate.of(1839, 6, 21))
+                .paisDeOrigem("Brasil")
+                .cpf("123.456.789-00")
+                .obras(List.of())
+                .build();
+
+    }
 }
